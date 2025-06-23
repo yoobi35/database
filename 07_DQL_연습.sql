d67652daf965ee0e0ee829c30849e9c0fa67b5fd
teacher-min <minkt78@gmail.com>
2025-06-23 17:27:01 +0900

    20250623

--- 07_DQL_연습.sql
+++ 07_DQL_연습.sql
@@ -2,25 +2,68 @@
 USE db_company;
 
 # 1. 직급이 "과장", "부장"인 직원 조회하기
-
+SELECT emp_id, dept_id, emp_name, position, gender, hire_date, salary
+  FROM tbl_employee
+ WHERE position IN ("과장", "부장");
+ 
 # 2. 급여가 3000000~5000000인 직원 조회하기
-
+SELECT emp_id, dept_id, emp_name, position, gender, hire_date, salary
+  FROM tbl_employee
+ WHERE salary BETWEEN 3000000 AND 5000000;
+ 
 # 3. 이름에 "민"이 포함된 직원 조회하기
-
-# 4. 직원들이 근무 중인 부서명을 중복 제거 후 조회하기
-
+SELECT emp_id, dept_id, emp_name, position, gender, hire_date, salary
+  FROM tbl_employee
+ WHERE emp_name LIKE "%민%";
+ 
+# 4. 직원들이 근무 중인 부서 아이디를 중복 제거 후 조회하기
+SELECT DISTINCT dept_id
+  FROM tbl_employee;
+  
+SELECT dept_id
+  FROM tbl_employee
+ GROUP BY dept_id;
+ 
 # 5. 직급별 급여 평균 조회하기
-
+SELECT position, AVG(salary)
+  FROM tbl_employee
+ GROUP BY position;
+ 
 # 6. 부서별 직원 수 조회하기
-
+SELECT dept_id AS 부서번호,
+       COUNT(*) AS 사원수
+  FROM tbl_employee
+ GROUP BY dept_id;
+ 
 # 7. 급여 평균이 5000000 이상인 직급 조회하기
+SELECT position
+  FROM tbl_employee
+ GROUP BY position
+HAVING AVG(salary) >= 5000000;
 
 # 8. 직급이 "과장"인 직원 수 조회하기
+SELECT COUNT(*)
+  FROM tbl_employee
+ WHERE position = "과장";
 
 # 9. 입사일 과거 순으로 조회하기
-
+SELECT emp_id, dept_id, emp_name, position, gender, hire_date, salary
+  FROM tbl_employee
+ ORDER BY hire_date ASC;
+ 
 # 10. 직급 순으로 조회하되, 같은 직급은 최신 고용 순으로 조회하기
+SELECT emp_id, dept_id, emp_name, position, gender, hire_date, salary
+  FROM tbl_employee
+ ORDER BY position ASC, hire_date DESC;
 
 # 11. 가장 급여가 높은 직원 1명 조회하기
-
+SELECT emp_id, dept_id, emp_name, position, gender, hire_date, salary
+  FROM tbl_employee
+ ORDER BY salary DESC
+ LIMIT 0, 1;
+ 
 # 12. 가장 먼저 입사한 직원 1명 조회하기
+SELECT emp_id, dept_id, emp_name, position, gender, hire_date, salary
+  FROM tbl_employee
+ ORDER BY hire_date ASC
+ LIMIT 0, 1;
