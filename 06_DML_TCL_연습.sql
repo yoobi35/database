c81f866933789ea7b98799cf689b72e393a8d9cc
teacher-min <minkt78@gmail.com>
2025-06-23 12:36:58 +0900

    20250623

--- 06_DML_TCL_연습.sql
+++ 06_DML_TCL_연습.sql
@@ -17,3 +17,64 @@
   1) 1번 고객이 2번 고객으로 100,000원을 계좌 이체하는 트랜잭션을 작성하고 실행합니다.
   2) 쿼리문 실행 중 발생하는 오류는 없다고 가정하고 ROLLBACK 처리는 하지 않습니다.
 */
+
+# db_bank 데이터베이스 생성
+CREATE DATABASE IF NOT EXISTS db_bank;
+
+# db_bank 데이터베이스 사용
+USE db_bank;
+
+# 은행 테이블 생성
+CREATE TABLE IF NOT EXISTS tbl_bank (
+  bank_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  bank_name VARCHAR(20) NOT NULL
+) ENGINE = InnoDB;
+
+# 고객 테이블 생성
+CREATE TABLE IF NOT EXISTS tbl_customer (
+  customer_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  customer_name VARCHAR(20) NOT NULL,
+  phone CHAR(11)
+) ENGINE = InnoDB;
+
+# 계좌 테이블 생성
+CREATE TABLE IF NOT EXISTS tbl_account (
+  account_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  bank_id INT NOT NULL,
+  customer_id INT NOT NULL,
+  balance DECIMAL(15,2) NOT NULL,
+  FOREIGN KEY(bank_id) REFERENCES tbl_bank(bank_id)
+    ON DELETE CASCADE,
+  FOREIGN KEY(customer_id) REFERENCES tbl_customer(customer_id)
+    ON DELETE CASCADE
+) ENGINE = InnoDB;
+
+# 샘플 데이터 입력
+INSERT INTO tbl_bank(bank_name)
+VALUES ("국민"),
+       ("기업");
+INSERT INTO tbl_customer(customer_name, phone)
+VALUES ("철수", "01000000000"),
+       ("영희", "01099999999");
+INSERT INTO tbl_account(bank_id, customer_id, balance)
+VALUES (1, 1, 500000),
+       (2, 2, 1000000);
+
+# 샘플 데이터 저장
+COMMIT;
+
+# 계좌 이체 트랜잭션 처리 (1번 계좌 -> 2번 계좌로 100,000원 이체)
+START TRANSACTION;
+
+# 1번 계좌에서 100,000 출금
+UPDATE tbl_account
+SET balance = balance - 100000
+WHERE account_id = 1;
+
+# 2번 계좌로 100,000 입금
+UPDATE tbl_account
+SET balance = balance + 100000
+WHERE account_id = 2;
+
+# 작업 완료
+COMMIT;
